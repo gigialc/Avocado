ed65a9418b16c73866895436363e083818a427d0
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-04-21 08:25:28 -0400

    push

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -25,12 +25,13 @@ export default function Home() {
             <h1 className="text-5xl sm:text-7xl font-bold text-yellow-500 px-4 sm:px-10 py-2 sm:py-8">
                 avocado health
                 <p className="text-black text-gray-700 text-xl"> 
-              
               </p>
-              </h1>
-             
+              </h1> 
               <p className="text-2xl font-bold text-center text-gray-800 mb-6">
-                ai copilot for accurate health content generation
+              Empowering health platforms with AI-driven content creation for faster, smarter, and cost-effective solutions.
+              </p>
+              <p className="text-2xl font-bold text-center text-gray-800 mb-6">
+                
               </p>
 
               <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-4 justify-center items-center ">
@@ -52,7 +53,7 @@ export default function Home() {
           <div className="bg-white shadow-xl rounded-lg p-4 w-full sm:w-64 flex flex-col justify-between ">
               <h3 className="text-lg font-bold text-purple-700 text-center">Accuracy</h3>
               <p className="text-sm text-gray-700 mt-2 flex-grow text-center">
-                Model output is based on well-established medical research and data.
+              Backed by the latest medical research for trustworthy content.
               </p>
           </div>
 
@@ -71,52 +72,34 @@ export default function Home() {
           </div>
       </div>
       <div className="w-full max-w-5xl mt-52 pb-32">
-          <h2 className="text-3xl font-bold text-center text-gray-800 mb-20">avocado health 3x user engagement and saves 100k/year in content writing</h2>
+          <h2 className="text-3xl font-bold text-center text-gray-800 mb-20">Revolutionizing health content with AI, boosting efficiency by up to 50% and speeding delivery by 20%.</h2>
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 text-center">
             <div className="py-5 px-4">
-              <p className="text-5xl font-bold text-purple-700 py-3">60%↑</p>
-              <p className="text-xl text-gray-600">User retention</p>
+              <p className="text-5xl font-bold text-purple-700 py-3">20%↑</p>
+              <p className="text-xl text-gray-600">Pipeline velocity</p>
             </div>
             <div className="py-5 px-4">
-              <p className="text-5xl font-bold text-purple-700 py-3">$100K↓</p>
-              <p className="text-xl text-gray-600">Cost of content writers</p>
+              <p className="text-5xl font-bold text-purple-700 py-3">50%↓</p>
+              <p className="text-xl text-gray-600">Content creation costs</p>
             </div>
             <div className="py-5 px-4">
               <p className="text-5xl font-bold text-purple-700 py-3">100%</p>
-              <p className="text-xl text-gray-600">Hallucination Free</p>
+              <p className="text-xl text-gray-600">Science-backed</p>
             </div>
           </div>
         </div>
       </div>
 
        {/* New Section inspired by the uploaded image */}
-       <div className="w-full max-w-5xl mx-auto my-5 p-10 pb-20 text-center">
-          <h2 className="text-4xl font-bold text-gray-800 mb-8">Get Set Up In No Time</h2>
-          <p className="text-xl text-gray-700 mb-8">Get your unique API and start using it in your health application.</p>
-          
-          {/* Optional: Add an image or a video here to visually represent the process */}
-          
-          <div className="flex flex-col sm:flex-row justify-center items-center gap-6">
-            <div className="bg-white p-6 rounded-lg shadow-xl">
-              <h3 className="text-2xl font-bold text-purple-700 mb-4">Quick Setup</h3>
-              <p className="text-gray-600">With our step-by-step guide, you will be up and running in less than 30 minutes.</p>
-            </div>
-            
-            <div className="bg-white p-6 rounded-lg shadow-xl">
-              <h3 className="text-2xl font-bold text-purple-700 mb-4">2 week trial</h3>
-              <p className="text-gray-600">If you do not like our API after your 2 week trial we guarantee a full refund.</p>
-            </div>
-            
-            <div className="bg-white p-6 rounded-lg shadow-xl">
-              <h3 className="text-2xl font-bold text-purple-700 mb-4">24/7 Support</h3>
-              <p className="text-gray-600">Our team is here to help anytime, with live chat and extensive resources.</p>
-            </div>
-          </div>
-          
-          <button className="mt-10 bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-3 px-8 rounded-lg" onClick={handleDemoClick}>
-            Book a Demo
-          </button>
-        </div>
+       <div className="flex flex-col justify-center items-center w-full mx-auto text-center pb-40">
+        {/* Content here will be fully centered, including on wider screens */}
+        <h2 className="text-4xl font-bold mb-8 text-black">Effortless Setup in Minutes</h2>
+        <p className="text-xl mb-8 text-black">Activate your API quickly and start enhancing your health apps today.</p>
+        <button className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 px-6 rounded-lg shadow-lg focus:outline-none focus:shadow-outline w-full sm:w-auto" onClick={handleDemoClick}>
+          Book a Demo
+        </button>
+      </div>
+
       </main>
       <Footer />
     </>
