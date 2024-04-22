4742188cf8fe357b1a1e7b684161c3c1966f629d
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-04-22 17:48:27 -0400

    push

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
               </p>
               </h1> 
               <p className="text-2xl font-bold text-center text-gray-800 mb-6">
-                AI-Driven Content Creation for Healthcare Marketing
+              The WebMD for personalized healthcare content
               </p>
               <p className="text-2xl font-bold text-center text-gray-800 mb-6">
                 
