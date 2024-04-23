b1f8b2f9ddef77202656ad1631cf7097efa8e674
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-04-22 20:15:05 -0400

    hi

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
               </p>
               </h1> 
               <p className="text-2xl font-bold text-center text-gray-800 mb-6">
-              The WebMD for personalized healthcare content
+              Personalized healthcare content at scale.
               </p>
               <p className="text-2xl font-bold text-center text-gray-800 mb-6">
                 
