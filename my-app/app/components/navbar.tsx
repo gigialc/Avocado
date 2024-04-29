6e982f10e2aeafe436bb111273d354b198abbf41
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-04-29 11:29:17 -0400

    busttons

--- my-app/app/components/navbar.tsx
+++ my-app/app/components/navbar.tsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.tsx
 import React, { useState } from 'react';
 import Link from 'next/link';
 
@@ -11,13 +10,11 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className="bg-transparent">
-          <div className="container px-6 py-3 mx-auto md:flex md:justify-between md:items-center">
-            <div className="flex items-center justify-between w-full">
-              
-              {/* Logo and Mobile Menu Button */}
-              <div>
-                <Link href="/" className="text-xl font-bold text-yellow-600 dark:text-yellow md:text-3xl">avocado</Link>
-              </div>
+          <div className="container px-6 py-3 mx-auto flex justify-between items-center">
+
+            {/* Logo and Mobile Menu Button */}
+            <div className="flex items-center justify-between">
+              <Link href="/" className="text-xl font-bold text-yellow-600 dark:text-yellow md:text-3xl">avocado</Link>
               
               <div className="md:hidden">
                 <button onClick={toggleMenu} className="text-gray-800 dark:text-gray-800 hover:text-gray-800 dark:hover:text-gray-800 focus:outline-none focus:text-gray-800 dark:focus:text-gray-800" aria-label="toggle menu">
@@ -30,29 +27,30 @@ const Navbar: React.FC = () => {
                   </svg>
                 </button>
               </div>
-              
-              {/* Desktop Links */}
-              <div className="hidden md:flex">
-                {/* <Link href="https://healthbyte.mintlify.app/api-reference/introduction" className="md:text-1xl my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4 my-0">API Docs</Link>  */}
-                <Link href="/api" className="md:text-1xl my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4 my-0">API Configuration</Link>
-                <Link href="/marketplace" className="md:text-1xl my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4 my-0">Marketplace</Link>
-                <Link href="/content-hub" className="my-1 text-sm font-medium text-gray-900 md:text-1xl dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4 my-0">Test API</Link> 
-                <a href="https://join.slack.com/t/avocadohealth-ai/shared_invite/zt-2gf0efdwn-Pfc9dwWT83aKHufeNNI5zA" className="my-1 text-sm font-medium text-gray-900 md:text-1xl dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4 my-0">Slack</a>
-              </div>
             </div>
-    
+            
+            {/* Desktop Links and Buttons aligned to the right */}
+            <div className="hidden md:flex items-center">
+              <Link href="/api" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4">API Configuration</Link>
+              <Link href="/marketplace" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4">Marketplace</Link>
+              <Link href="/content-hub" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4">Test API</Link> 
+              <a href="https://join.slack.com/t/avocadohealth-ai/shared_invite/zt-2gf0efdwn-Pfc9dwWT83aKHufeNNI5zA" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-purple-800 mx-4">Slack</a>
+              <Link href="/login" className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mx-2">Sign In</Link>
+              <Link href="/sign-up" className="bg-purple-800 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded mx-2">Sign Up</Link>
+            </div>
+            
+            {/* Mobile Menu that appears when isOpen is true */}
             {isOpen && (
-              <div className="flex flex-col items-center md:flex-row md:mx-6">
-                <Link href="/" className="my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 md:mx-4 md:my-0" onClick={() => setIsOpen(false)}>Home</Link>
-                {/* <Link href="https://healthbyte.mintlify.app/api-reference/introduction" className="my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 md:mx-4 md:my-0" onClick={() => setIsOpen(false)}>API Docs</Link> */}
-                <Link href="/api" className="my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 md:mx-4 md:my-0" onClick={() => setIsOpen(false)}>API Configuration</Link>
-                <Link href="/marketplace" className="my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 md:mx-4 md:my-0" onClick={() => setIsOpen(false)}>Marketplace</Link>
-                <Link href="/content-hub" className="my-1 text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 md:mx-4 md:my-0" onClick={() => setIsOpen(false)}>Try</Link>
+              <div className="flex flex-col items-center md:hidden">
+                <Link href="/" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 mx-4" onClick={() => setIsOpen(false)}>Home</Link>
+                <Link href="/api" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 mx-4" onClick={() => setIsOpen(false)}>API Configuration</Link>
+                <Link href="/marketplace" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 mx-4" onClick={() => setIsOpen(false)}>Marketplace</Link>
+                <Link href="/content-hub" className="text-sm font-medium text-gray-900 dark:text-gray-800 hover:text-purple-700 dark:hover:text-gray-200 mx-4" onClick={() => setIsOpen(false)}>Try</Link>
               </div>
-            )} 
+            )}
           </div>
         </nav>
       );
-    };
+};
 
 export default Navbar;
