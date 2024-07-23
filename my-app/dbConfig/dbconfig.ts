19decead33c21bcf896a45a03dfdb72a6367d05b
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-07-23 00:22:13 -0400

    changes

--- my-app/dbConfig/dbconfig.ts
+++ my-app/dbConfig/dbconfig.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
 export async function connectToMongoDB() {
+	console.log('Connecting to MongoDB...');
+	console.log('process.env.MONGO_URI', process.env.MONGO_URI);
 	try {
 		mongoose.connect(process.env.MONGO_URI!);
 		const connection = mongoose.connection;
