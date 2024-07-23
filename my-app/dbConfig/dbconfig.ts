45323a9c02daef0c8a0caddd42c89f10f27b1808
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-07-23 00:36:54 -0400

    changes

--- my-app/dbConfig/dbconfig.ts
+++ my-app/dbConfig/dbconfig.ts
@@ -1,21 +1,28 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();  // Load environment variables from .env file
 
 export async function connectToMongoDB() {
-	console.log('Connecting to MongoDB...');
-	console.log('process.env.MONGO_URI', process.env.MONGO_URI);
-	try {
-		mongoose.connect(process.env.MONGO_URI!);
-		const connection = mongoose.connection;
+    try {
+        const uri = process.env.MONGO_URI;
+        
+        if (!uri) {
+            throw new Error('MONGO_URI is not defined');
+        }
 
-		connection.on('connected', () => {
-			console.log('Great! MongoDb is connected girl!');
-		});
+		mongoose.connect(uri, { });  // Removed useNewUrlParser and useUnifiedTopology
+        const connection = mongoose.connection;
 
-		connection.on('error', (err) => {
-			console.log('MongoDB connected ERROR. ' + err);
-			process.exit();
-		});
-	} catch (error) {
-		console.log('Ups! Something went wrong! ' + error);
-	}
+        connection.on('connected', () => {
+            console.log('Great! MongoDb is connected girl!');
+        });
+
+        connection.on('error', (err) => {
+            console.log('MongoDB connected ERROR. ' + err);
+            process.exit();
+        });
+    } catch (error) {
+        console.log('Ups! Something went wrong! ' + error);
+    }
 }
